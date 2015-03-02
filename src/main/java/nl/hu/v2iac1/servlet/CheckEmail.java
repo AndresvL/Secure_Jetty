60ec39c4842ebf2fb471b3718f0268fcda106ee2
Andres van Lummel <andresvanlummel@hotmail.com>
2015-03-02 20:50:40 +0100

    lol

--- src/main/java/nl/hu/v2iac1/servlet/CheckEmail.java
+++ src/main/java/nl/hu/v2iac1/servlet/CheckEmail.java
@@ -10,16 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 import nl.hu.v2iac1.domein.User;
 
 public class CheckEmail extends HttpServlet {
-
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String key = request.getParameter("key");
-		System.out.println("This is the key: " + key);
 		User u = (User) request.getSession().getAttribute("topsecretuser");
 		if (u.getKey().equals(key)) {
 			response.sendRedirect("/Secure_Jetty/rest/topsecret/");
 		} else {
-			response.sendRedirect("/Secure_Jetty/loginTopsecret.jsp");
+			response.sendRedirect("/Secure_Jetty/checkEmailkey.jsp");
 			request.getServletContext().setAttribute("user", null);
 		}
 	}
