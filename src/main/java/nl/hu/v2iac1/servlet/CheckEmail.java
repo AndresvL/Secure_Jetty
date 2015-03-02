5d8f4107700ae30db1ef60ed011b2f92f6ef0fab
Andres van Lummel <andresvanlummel@hotmail.com>
2015-03-02 20:57:37 +0100

    update again

--- src/main/java/nl/hu/v2iac1/servlet/CheckEmail.java
+++ src/main/java/nl/hu/v2iac1/servlet/CheckEmail.java
@@ -2,6 +2,7 @@ package nl.hu.v2iac1.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,9 @@ public class CheckEmail extends HttpServlet {
 		if (u.getKey().equals(key)) {
 			response.sendRedirect("/Secure_Jetty/rest/topsecret/");
 		} else {
-			response.sendRedirect("/Secure_Jetty/checkEmailkey.jsp");
+			RequestDispatcher rd = null;
+			rd = request.getRequestDispatcher("/Secure_Jetty/checkEmailkey.jsp");
+			request.setAttribute("msgs", "De ingevulde code komt niet overeen met die in de mail");
 			request.getServletContext().setAttribute("user", null);
 		}
 	}
